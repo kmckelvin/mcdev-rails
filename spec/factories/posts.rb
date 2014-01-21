4c07a714301b93e3ac9fe0421051067ada1ed8be
Kevin McKelvin <kmckelvin@gmail.com>
2014-01-22 00:52:33 +0200

    Added slug generation to the Post factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :post do
     sequence(:title) { |n| "The post #{n}" }
+    sequence(:slug)  { |n| "slug-#{n}" }
     body "# Heading"
     teaser "This is a post teaser"
 
